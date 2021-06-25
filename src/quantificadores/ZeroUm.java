7d139ac8482ea231fbe6ed4abb772b3ac5eacedb
Daniel Dutra <danieldhsd@gmail.com>
2021-06-24 21:42:20 -0300

    Zero ou um

---
+++ src/quantificadores/ZeroUm.java
@@ -0,0 +1,17 @@
+package quantificadores;
+
+import utilities.Util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ZeroUm {
+	
+	public static void main(String[] args) {
+		String texto = "De longe eu avistei o fogo e uma pessoa gritando: FOGOOOO!";
+		String texto1 = "There is a big fog in NYC";
+		
+		Util.imprimeResultado(Pattern.compile("fogo?", Pattern.CASE_INSENSITIVE).matcher(texto));
+		Util.imprimeResultado(Pattern.compile("fogo?", Pattern.CASE_INSENSITIVE).matcher(texto1));
+	}
+}
