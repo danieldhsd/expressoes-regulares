2afcdeeb7c7793ec0dc6b4bf988eb323411aae83
Daniel Dutra <danieldhsd@gmail.com>
2021-06-26 10:11:54 -0300

    Zero ou mais

---
+++ src/quantificadores/ZeroMais.java
@@ -0,0 +1,16 @@
+package quantificadores;
+
+import utilities.Util;
+
+import java.util.regex.Pattern;
+
+public class ZeroMais {
+	
+	public static void main(String[] args) {
+		String texto = "De longe eu avistei o fogo e uma pessoa gritando: FOGOOOO!";
+		String texto1 = "There is a big fog in NYC";
+		
+		Util.imprimeResultado(Pattern.compile("fogo*", Pattern.CASE_INSENSITIVE).matcher(texto));
+		Util.imprimeResultado(Pattern.compile("fogo*", Pattern.CASE_INSENSITIVE).matcher(texto1));
+	}
+}
