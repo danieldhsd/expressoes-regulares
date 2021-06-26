c56eb24d8b43fa2fb6d1967f58408d96ef4b47e2
Daniel Dutra <danieldhsd@gmail.com>
2021-06-26 10:10:03 -0300

    Um ou mais

--- src/quantificadores/ZeroUm.java
+++ src/quantificadores/ZeroUm.java
@@ -2,7 +2,6 @@ package quantificadores;
 
 import utilities.Util;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ZeroUm {
--- src/utilities/Util.java
+++ src/utilities/Util.java
@@ -6,8 +6,7 @@ public class Util {
 	
 	public static void imprimeResultado(Matcher matcher) {
 		while (matcher.find()) {
-			System.out.printf("Posicoes: %s, %s\tValor: %s%n",
-					matcher.start(), matcher.end(), matcher.group());
+			System.out.printf("Valor: %s%n", matcher.group());
 		}
 		
 		System.out.println("-------------------");
